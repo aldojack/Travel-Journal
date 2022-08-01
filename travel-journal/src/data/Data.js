ed162b570c0380cd5dedf4ca3546375362aaa7f2
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-08-01 22:00:02 +0100

    Trip data and images added

---
+++ travel-journal/src/data/Data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        city: "Seville",
+        country: "Spain",
+        googleMapsUrl: "https://goo.gl/maps/jzbaZQLCTYFGWLks6",
+        startDate: "17 May, 2022",
+        endDate: "19 May, 2022",
+        description: "Stayed in Malaga for 2 nights with a day trip to Seville to watch Glasgow Rangers vs Eintracht Frankfurt at the Ramon Sanchez-Pizjuan Stadium, Home of Sevilla FC",
+        imageUrl: "/Seville.jpg"
+    },
+    {
+        city: "Kraków",
+        country: "Poland",
+        googleMapsUrl: "https://goo.gl/maps/An9CPMZtijoVcmhF8",
+        startDate: "7 Feb 2020",
+        endDate: "10 Feb 2020",
+        description: "3 night trip to Krakow for Laura's birthday.  We visited Auschwitz camp and spent the day on site and also fired guns at a shooting range on our last day.",
+        imageUrl: "/Krakow.jpg"
+    },,
+    {
+        city: "Denver",
+        country: "USA",
+        googleMapsUrl: "https://goo.gl/maps/f4pe2Uj1WuJhvFkm7",
+        startDate: "8 Nov 2018",
+        endDate: "12 Nov 2018",
+        description: "As part of a UFC competition win to fly to Denver for the 25th anniversary of the UFC.  VIP booth with all inclusive bar, very messy night!",
+        imageUrl: "/Denver.jpg"
+    }
+]
